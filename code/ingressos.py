b212e4de3d4dd16db9f3cba86646e07cbe99f28b
fsantanna <francisco.santanna@gmail.com>
2016-02-27 09:44:03 -0300

    (+) ingressos

---
+++ code/ingressos.py
@@ -0,0 +1,11 @@
+ingressos = [ 60, 35, 130, 50, 15 ]
+
+total = 0
+
+for preco in ingressos:
+    total = total + preco
+
+print "Total: ", total
+
+media = total/len(ingressos)
+print "Media: ", media
