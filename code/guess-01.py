f323ddde88d1f82acda0d899fa8f23caafc9600b
Francisco Sant'Anna <francisco.santanna@gmail.com>
2016-08-28 10:27:05 -0300

    ...

---
+++ code/guess-01.py
@@ -0,0 +1,7 @@
+print "Bem-vindo ao jogo..."
+guess = raw_input("Adivinhe o numero: ")
+if guess == "5":
+    print "Voce ganhou!"
+else:
+    print "Voce perdeu..."
+print "Fim"
---
+++ code/guess-02.py
@@ -0,0 +1,8 @@
+print "Bem-vindo ao jogo..."
+g = raw_input("Adivinhe o numero: ")
+guess = int(g)
+if guess == 5:
+    print "Voce ganhou!"
+else:
+    print "Voce perdeu..."
+print "Fim"
